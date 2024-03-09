cbe5fec4d2743f346b89329f9e91676118802180
sanchxt <tpbeast007@gmail.com>
2024-03-09 19:44:04 +0530

    setup boilerplate router

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,5 +1,19 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+
+import { Home, Profile, SignUp, SignIn, About } from "./pages";
+
 const App = () => {
-  return <h1 className="text-8xl text-red-500">App</h1>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/sign-in" element={<SignIn />} />
+        <Route path="/sign-up" element={<SignUp />} />
+        <Route path="/profile" element={<Profile />} />
+      </Routes>
+    </BrowserRouter>
+  );
 };
 
 export default App;
