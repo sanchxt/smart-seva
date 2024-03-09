bc01a0df64100cd84c49cc53d97f54cf8f773464
sanchxt <tpbeast007@gmail.com>
2024-03-10 00:56:14 +0530

    error handling for signup api

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -1,8 +1,9 @@
 import bcryptjs from "bcryptjs";
 
 import User from "../models/user.model.js";
+import { errorHandler } from "../utils/error.js";
 
-export const signup = async (req, res) => {
+export const signup = async (req, res, next) => {
   const { username, email, password, isAdminAccount } = req.body;
   const hashedPassword = bcryptjs.hashSync(password, 10);
 
@@ -17,6 +18,6 @@ export const signup = async (req, res) => {
     await newUser.save();
     res.status(201).json({ message: "User created successfully" });
   } catch (error) {
-    res.status(500).json(error.message);
+    next(error);
   }
 };
