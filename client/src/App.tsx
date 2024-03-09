c9c7d74ab0e3ccea927c76547cde56a1a77133d6
sanchxt <tpbeast007@gmail.com>
2024-03-09 20:00:14 +0530

    setup navbar

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,10 +1,12 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 import { Home, Profile, SignUp, SignIn, About } from "./pages";
+import Header from "./components/Header";
 
 const App = () => {
   return (
     <BrowserRouter>
+      <Header />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
