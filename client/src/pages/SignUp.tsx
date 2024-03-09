183b7845aead4ad444a9936c8f090f0be30ad837
sanchxt <tpbeast007@gmail.com>
2024-03-10 01:33:54 +0530

    create signup form

--- client/src/pages/SignUp.tsx
+++ client/src/pages/SignUp.tsx
@@ -1,5 +1,43 @@
 const SignUp = () => {
-  return <div>SignUp</div>;
+  return (
+    <div className="p-3 max-w-lg mx-auto">
+      <h1 className="text-3xl text-center font-semibold my-7">Sign Up</h1>
+
+      <form className="flex flex-col gap-4">
+        <input
+          type="text"
+          placeholder="Your username here"
+          id="username"
+          className="bg-slate-100 p-3 rounded-lg"
+        />
+        <input
+          type="email"
+          placeholder="Your email here"
+          id="email"
+          className="bg-slate-100 p-3 rounded-lg"
+        />
+        <input
+          type="password"
+          placeholder="Your password here"
+          id="password"
+          className="bg-slate-100 p-3 rounded-lg"
+        />
+        <input
+          type="text"
+          placeholder="Here to monitor someone? Enter their key..."
+          id="isAdminAccount"
+          className="bg-slate-100 p-3 rounded-lg"
+        />
+
+        <button
+          type="submit"
+          className="bg-slate-700 opacity-[96%] text-white p-3 rounded-lg uppercase hover:bg-slate-800 hover:opacity-100"
+        >
+          Sign Up
+        </button>
+      </form>
+    </div>
+  );
 };
 
 export default SignUp;
