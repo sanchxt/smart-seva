ba29acf8bfd6658e42ce983ce5a0387398b0c575
sanchxt <tpbeast007@gmail.com>
2024-03-11 01:42:10 +0530

    use firebase api

--- client/src/components/OAuth.tsx
+++ client/src/components/OAuth.tsx
@@ -1,6 +1,25 @@
+import axios from "axios";
+import { app } from "../firebase";
+import { useDispatch } from "react-redux";
+import { signInSuccess } from "../redux/user/userSlice";
+import { GoogleAuthProvider, signInWithPopup, getAuth } from "firebase/auth";
+
 const OAuth = () => {
+  const dispatch = useDispatch();
+
   const handleGoogleClick = async () => {
     try {
+      const provider = new GoogleAuthProvider();
+      const auth = getAuth(app);
+      const result = await signInWithPopup(auth, provider);
+      const requestBody = {
+        name: result.user.displayName,
+        email: result.user.email,
+        photo: result.user.photoURL,
+      };
+      const response = await axios.post("/api/auth/google", requestBody);
+
+      dispatch(signInSuccess(response.data));
     } catch (error) {
       console.log("Couldn't login with Google", error);
     }
