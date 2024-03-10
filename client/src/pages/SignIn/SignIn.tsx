0df486e0dfe01482ffec5a555437927e7fdd304f
sanchxt <tpbeast007@gmail.com>
2024-03-10 23:59:25 +0530

    added redux

--- client/src/pages/SignIn/SignIn.tsx
+++ client/src/pages/SignIn/SignIn.tsx
@@ -1,54 +1,128 @@
+import axios from "axios";
 import { Link, useNavigate } from "react-router-dom";
+import { useSelector, useDispatch } from "react-redux";
 
-import HandleSubmit from "./HandleSubmit";
-import { useHandleChange } from "./HandleChange";
+import {
+  signInFailure,
+  signInStart,
+  signInSuccess,
+} from "../../redux/user/userSlice";
 import { inputFields } from "./inputFields";
-import { useState } from "react";
+import { useHandleChange } from "./HandleChange";
 
 const SignIn = () => {
-  const { formData, handleChange } = useHandleChange();
-  const [loading, setLoading] = useState(false);
-  const [error, setError] = useState(false);
   const navigate = useNavigate();
+  const dispatch = useDispatch();
+  const { formData, handleChange } = useHandleChange();
+  const { loading, error } = useSelector((state: any) => state.user);
 
-  const handleSubmit = async (e: { preventDefault: () => void }) => {
-    await HandleSubmit(e, formData, setLoading, setError, navigate);
+  const handleSubmit = async (e: { preventDefault(): void }) => {
+    e.preventDefault();
+    try {
+      dispatch(signInStart());
+      const response = await axios.post("/api/auth/sign-in", formData);
+      dispatch(signInSuccess(response));
+      navigate("/");
+    } catch (error: any) {
+      dispatch(signInFailure(error));
+    }
   };
 
   return (
     <div className="p-3 max-w-lg mx-auto">
       <h1 className="text-3xl text-center font-semibold my-7">Sign In</h1>
-
       <form onSubmit={handleSubmit} className="flex flex-col gap-4">
-        {inputFields.map((field) => (
-          <input
-            key={field.id}
-            {...field}
-            className="bg-slate-100 p-3 rounded-lg"
-            onChange={handleChange}
-          />
-        ))}
-
+        {inputFields.map(
+          (field: { placeholder: string; type: string; id: string }) => (
+            <input
+              key={field.id}
+              {...field}
+              className="bg-slate-100 p-3 rounded-lg"
+              onChange={handleChange}
+            />
+          )
+        )}
         <button
           type="submit"
-          className="bg-slate-700 opacity-[96%] text-white p-3 rounded-lg uppercase hover:bg-slate-800 hover:opacity-100 disabled:opacity-70 disabled:bg-slate-700"
           disabled={loading}
+          className="bg-slate-700 opacity-[96%] text-white p-3 rounded-lg uppercase hover:bg-slate-800 hover:opacity-100 disabled:opacity-70 disabled:bg-slate-700"
         >
           {loading ? "Loading..." : "Sign In"}
         </button>
       </form>
-
-      <div className="flex gap-2 mt-3">
+      <div className="flex gap-2 mt-5">
         <p>Don&apos;t have an account?</p>
         <Link to="/sign-up">
           <span className="text-blue-500">Sign Up</span>
         </Link>
       </div>
-      <p className="text-red-700 mt-5 text-2xl text-center font-semibold">
-        {error && "Wrong credentials"}
+      <p className="text-red-700 mt-5 text-center font-semibold">
+        {error
+          ? "Error: " + error.response.data.message || "An error occurred"
+          : ""}
       </p>
     </div>
   );
 };
 
 export default SignIn;
+
+// const SignIn = () => {
+//   const { formData, handleChange } = useHandleChange();
+//   // const [loading, setLoading] = useState(false);
+//   // const [error, setError] = useState(false);
+//   const { loading, error } = useSelector((state: any) => state.user);
+//   const navigate = useNavigate();
+
+//   const handleSubmit = async (e: { preventDefault: () => void }) => {
+//     const dispatch = useDispatch();
+//     console.log(
+//       "Initial state:",
+//       useSelector((state: any) => state.user)
+//     );
+//     try {
+//       await HandleSubmit(e, formData, navigate, dispatch);
+//       console.log(
+//         "State after signInStart:",
+//         useSelector((state: any) => state.user)
+//       );
+//     } catch (err) {
+//       console.error(err);
+//     }
+//   };
+
+//   return (
+//     <div className="p-3 max-w-lg mx-auto">
+//       <h1 className="text-3xl text-center font-semibold my-7">Sign In</h1>
+
+//       <form onSubmit={handleSubmit} className="flex flex-col gap-4">
+//         {inputFields.map((field) => (
+//           <input
+//             key={field.id}
+//             {...field}
+//             className="bg-slate-100 p-3 rounded-lg"
+//             onChange={handleChange}
+//           />
+//         ))}
+
+//         <button
+//           type="submit"
+//           className="bg-slate-700 opacity-[96%] text-white p-3 rounded-lg uppercase hover:bg-slate-800 hover:opacity-100 disabled:opacity-70 disabled:bg-slate-700"
+//           disabled={loading}
+//         >
+//           {loading ? "Loading..." : "Sign In"}
+//         </button>
+//       </form>
+
+//       <div className="flex gap-2 mt-3">
+//         <p>Don&apos;t have an account?</p>
+//         <Link to="/sign-up">
+//           <span className="text-blue-500">Sign Up</span>
+//         </Link>
+//       </div>
+//       <p className="text-red-700 mt-5 text-2xl text-center font-semibold">
+//         {error && "Wrong credentials"}
+//       </p>
+//     </div>
+//   );
+// };
---
+++ client/src/redux/user/userSlice.ts
@@ -0,0 +1,35 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+interface UserState {
+  currentUser: any;
+  loading: boolean;
+  error: boolean;
+}
+
+const initialState: UserState = {
+  currentUser: null,
+  loading: false,
+  error: false,
+};
+
+const userSlice = createSlice({
+  name: "user",
+  initialState,
+  reducers: {
+    signInStart: (state) => {
+      state.loading = true;
+    },
+    signInSuccess: (state, action) => {
+      state.currentUser = action.payload;
+      state.loading = false;
+      state.error = false;
+    },
+    signInFailure: (state, action) => {
+      state.loading = false;
+      state.error = action.payload;
+    },
+  },
+});
+
+export const { signInStart, signInSuccess, signInFailure } = userSlice.actions;
+export default userSlice.reducer;
