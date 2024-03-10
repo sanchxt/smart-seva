25297f974f3528e7395a41fadecd33b064476e01
sanchxt <tpbeast007@gmail.com>
2024-03-11 00:56:26 +0530

    integrated redux persist

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,12 +1,15 @@
-import ReactDOM from "react-dom/client";
 import { Provider } from "react-redux";
+import ReactDOM from "react-dom/client";
+import { persistor, store } from "./redux/store.ts";
+import { PersistGate } from "redux-persist/integration/react";
 
-import { store } from "./redux/store.ts";
 import App from "./App.tsx";
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <Provider store={store}>
-    <App />
+    <PersistGate persistor={persistor} loading={null}>
+      <App />
+    </PersistGate>
   </Provider>
 );
