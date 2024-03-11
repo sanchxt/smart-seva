7a7a402e8a0b8caeb9bd74636ea13fc667b3daf4
sanchxt <tpbeast007@gmail.com>
2024-03-11 23:46:05 +0530

    update profile ui

--- client/src/pages/Profile.tsx
+++ client/src/pages/Profile.tsx
@@ -1,5 +1,52 @@
+import { useSelector } from "react-redux";
+
 const Profile = () => {
-  return <div>Profile</div>;
+  const { currentUser } = useSelector((state: any) => state.user);
+
+  return (
+    <div className="p-3 max-w-lg mx-auto">
+      <h1 className="text-3xl font-semibold text-center my-7">Profile</h1>
+      <form className="flex flex-col gap-4">
+        <img
+          src={currentUser.profilePicture}
+          alt="profile"
+          className="h-24 w-24 self-center cursor-pointer rounded-full object-cover mt-2"
+        />
+
+        <input
+          type="text"
+          defaultValue={currentUser.username}
+          id="username"
+          placeholder="Username"
+          className="bg-slate-100 rounded-lg p-3"
+        />
+        <input
+          type="email"
+          defaultValue={currentUser.email}
+          id="email"
+          placeholder="E-mail"
+          className="bg-slate-100 rounded-lg p-3"
+        />
+        <input
+          type="password"
+          id="password"
+          placeholder="Password"
+          className="bg-slate-100 rounded-lg p-3"
+        />
+        <button className="bg-slate-700 opacity-95 text-white p-3 rounded-lg uppercase hover:bg-slate-800 hover:opacity-100 disabled:opacity-80">
+          Update
+        </button>
+      </form>
+      <div className="flex justify-between mt-5">
+        <span className="text-red-700 cursor-pointer font-semibold hover:font-bold">
+          Delete Account
+        </span>
+        <span className="text-red-700 cursor-pointer font-semibold hover:font-bold">
+          Sign Out
+        </span>
+      </div>
+    </div>
+  );
 };
 
 export default Profile;
