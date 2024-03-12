3d70db1b61aead0a18157fcf5f792b598384436d
sanchxt <tpbeast007@gmail.com>
2024-03-12 22:02:13 +0530

    add delete slices

--- client/src/redux/user/userSlice.ts
+++ client/src/redux/user/userSlice.ts
@@ -40,6 +40,18 @@ const userSlice = createSlice({
       state.loading = false;
       state.error = action.payload;
     },
+    deleteUserStart: (state) => {
+      state.loading = true;
+    },
+    deleteUserSuccess: (state) => {
+      state.currentUser = null;
+      state.loading = false;
+      state.error = false;
+    },
+    deleteUserFailure: (state, action) => {
+      state.loading = false;
+      state.error = action.payload;
+    },
   },
 });
 
@@ -50,6 +62,9 @@ export const {
   updateUserStart,
   updateUserSuccess,
   updateUserFailure,
+  deleteUserStart,
+  deleteUserSuccess,
+  deleteUserFailure,
 } = userSlice.actions;
 
 export default userSlice.reducer;
