4c6404118af06c9a5e66275f4c422e58a3152c24
sanchxt <tpbeast007@gmail.com>
2024-03-12 22:41:49 +0530

    run some tests on about page

--- client/src/pages/About.tsx
+++ client/src/pages/About.tsx
@@ -1,5 +1,10 @@
 const About = () => {
-  return <div>About</div>;
+  return (
+    <div className="px-4 py-12 max-w-2xl mx-auto">
+      <h1 className="text-3xl font-bold mb-4 text-slate-800">About</h1>
+      <p className="mb-4 text-slate-700">Lorem ipsum about page section</p>
+    </div>
+  );
 };
 
 export default About;
