5f12c541609ca03e3f741cfc8ae84fab582fabeb
sanchxt <tpbeast007@gmail.com>
2024-03-12 22:41:56 +0530

    run some tests on home page

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -1,5 +1,10 @@
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <div className="px-4 py-12 max-w-2xl mx-auto">
+      <h1 className="text-3xl font-bold mb-4 text-slate-800">Home Page</h1>
+      <p className="mb-4 text-slate-700">Lorem ipsum home page section</p>
+    </div>
+  );
 };
 
 export default Home;
