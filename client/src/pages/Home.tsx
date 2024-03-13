500b8f8d745d4a6087effb3a1656a59cdfce4b3e
sanchxt <tpbeast007@gmail.com>
2024-03-13 10:06:23 +0530

    update text

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -2,10 +2,6 @@ import Header from "../components/Header";
 
 const Home = () => {
   return (
-    // <div className="px-4 py-12 max-w-2xl mx-auto">
-    //   <h1 className="text-3xl font-bold mb-4 text-slate-800">Home Page</h1>
-    //   <p className="mb-4 text-slate-700">Lorem ipsum home page section</p>
-    // </div>
     <>
       <Header />
       <div className="overflow-hidden h-80">
