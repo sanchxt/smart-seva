c2cc6ad4e9ba634f6027c7dfce5c4244242d9343
sanchxt <tpbeast007@gmail.com>
2024-03-13 20:04:05 +0530

    add sidebar

--- client/src/pages/Home/Home.tsx
+++ client/src/pages/Home/Home.tsx
@@ -1,22 +1,27 @@
-import Header from "../../components/Header";
+import { Link } from "react-router-dom";
+import Sidebar, { SidebarItem } from "./Sidebar";
+import { LayoutDashboard, Home, StickyNote, Layers, Flag, Calendar, LifeBuoy, Settings } from "lucide-react";
 
-const Home = () => {
+const HomePage = () => {
   return (
     <>
-      <Header />
-      <div className="overflow-hidden h-80">
-        <div className="flex gap-2">
-          <div className="bg-yellow-400 lg:h-screen lg:w-[300px] overflow-hidden">
-            <p className="uppercase text-6xl text-center">sidebar</p>
-          </div>
-
-          <div className="bg-yellow-400 lg:h-screen lg:w-screen overflow-hidden rounded-lg">
-            <p className="uppercase text-6xl text-center">sidebar</p>
-          </div>
-        </div>
+      <div className="flex">
+        <Sidebar>
+          <SidebarItem icon={<Home size={20} />} text="Home" alert />
+          <SidebarItem icon={<LayoutDashboard size={20} />} text="Dashboard" active />
+          <SidebarItem icon={<StickyNote size={20} />} text="Projects" alert />
+          <SidebarItem icon={<Calendar size={20} />} text="Calendar" />
+          <SidebarItem icon={<Layers size={20} />} text="Tasks" />
+          <SidebarItem icon={<Flag size={20} />} text="Reporting" />
+          <hr className="my-3" />
+          <Link to='/profile'>
+            <SidebarItem icon={<Settings size={20} />} text="Settings" />
+          </Link>
+          <SidebarItem icon={<LifeBuoy size={20} />} text="Help" />
+        </Sidebar>
       </div>
     </>
   );
 };
 
-export default Home;
+export default HomePage;
