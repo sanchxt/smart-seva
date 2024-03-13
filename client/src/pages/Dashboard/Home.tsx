b3e013a56b42bbf7a82f342ef839201d87430b14
sanchxt <tpbeast007@gmail.com>
2024-03-13 22:39:56 +0530

    change code structure

--- client/src/pages/Dashboard/Home.tsx
+++ client/src/pages/Dashboard/Home.tsx
@@ -1,6 +1,15 @@
 import { Link } from "react-router-dom";
 import Sidebar, { SidebarItem } from "./Sidebar";
-import { LayoutDashboard, Home, StickyNote, Layers, Flag, Calendar, LifeBuoy, Settings } from "lucide-react";
+import {
+  LayoutDashboard,
+  Home,
+  StickyNote,
+  Layers,
+  Flag,
+  Calendar,
+  LifeBuoy,
+  Settings,
+} from "lucide-react";
 
 const HomePage = () => {
   return (
@@ -8,19 +17,25 @@ const HomePage = () => {
       <div className="flex">
         <Sidebar>
           <SidebarItem icon={<Home size={20} />} text="Home" alert />
-          <SidebarItem icon={<LayoutDashboard size={20} />} text="Dashboard" active />
+          <SidebarItem
+            icon={<LayoutDashboard size={20} />}
+            text="Dashboard"
+            active
+          />
           <SidebarItem icon={<StickyNote size={20} />} text="Projects" alert />
           <SidebarItem icon={<Calendar size={20} />} text="Calendar" />
           <SidebarItem icon={<Layers size={20} />} text="Tasks" />
           <SidebarItem icon={<Flag size={20} />} text="Reporting" />
           <hr className="my-3" />
-          <Link to='/profile'>
+          <Link to="/profile">
             <SidebarItem icon={<Settings size={20} />} text="Settings" />
           </Link>
           <SidebarItem icon={<LifeBuoy size={20} />} text="Help" />
         </Sidebar>
       </div>
     </>
+
+    // <div>Dashboard</div>
   );
 };
 
