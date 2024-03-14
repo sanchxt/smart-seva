4e01dda6826d096c9940e75ecf7964b008d39fc9
sanchxt <tpbeast007@gmail.com>
2024-03-14 14:04:55 +0530

    add more files for exporting

--- client/src/pages/index.ts
+++ client/src/pages/index.ts
@@ -1,5 +1,6 @@
 export { default as About } from "./About";
-export { default as Home } from "./Dashboard/Home";
 export { default as Profile } from "./Profile";
+export { default as Home } from "./Dashboard/Home";
 export { default as SignUp } from "./SignUp/SignUp";
 export { default as SignIn } from "./SignIn/SignIn";
+export { default as UploadPrescription } from "./UploadPrescription";
