8fe4e1cfc1cf9e367c6d6ef88cff3378ef8a895d
sanchxt <tpbeast007@gmail.com>
2024-03-14 14:06:44 +0530

    setup page

--- client/src/pages/Dashboard/Home.tsx
+++ client/src/pages/Dashboard/Home.tsx
@@ -1,41 +1,11 @@
-import { Link } from "react-router-dom";
-import Sidebar, { SidebarItem } from "./Sidebar";
-import {
-  LayoutDashboard,
-  Home,
-  StickyNote,
-  Layers,
-  Flag,
-  Calendar,
-  LifeBuoy,
-  Settings,
-} from "lucide-react";
+import SidebarItems from "../../components/SidebarItems";
 
 const HomePage = () => {
   return (
-    <>
-      <div className="flex">
-        <Sidebar>
-          <SidebarItem icon={<Home size={20} />} text="Home" alert />
-          <SidebarItem
-            icon={<LayoutDashboard size={20} />}
-            text="Dashboard"
-            active
-          />
-          <SidebarItem icon={<StickyNote size={20} />} text="Projects" alert />
-          <SidebarItem icon={<Calendar size={20} />} text="Calendar" />
-          <SidebarItem icon={<Layers size={20} />} text="Tasks" />
-          <SidebarItem icon={<Flag size={20} />} text="Reporting" />
-          <hr className="my-3" />
-          <Link to="/profile">
-            <SidebarItem icon={<Settings size={20} />} text="Settings" />
-          </Link>
-          <SidebarItem icon={<LifeBuoy size={20} />} text="Help" />
-        </Sidebar>
-      </div>
-    </>
-
-    // <div>Dashboard</div>
+    <div className="flex flex-row">
+      <SidebarItems activeClass="dashboard" />
+      <h1>hi</h1>
+    </div>
   );
 };
 
