0e4a1824310b51583b9ace64ddec113920c17af2
sanchxt <tpbeast007@gmail.com>
2024-03-14 14:09:23 +0530

    add upload prescription route

--- backend/routes/user.route.js
+++ backend/routes/user.route.js
@@ -4,6 +4,7 @@ import {
   deleteUser,
   test,
   updateUser,
+  upload,
 } from "../controllers/user.controller.js";
 import { verifyToken } from "../utils/verifyUser.js";
 
@@ -11,6 +12,7 @@ const router = express.Router();
 
 router.get("/", test);
 router.post("/update/:id", verifyToken, updateUser);
+router.post("/upload/:id", verifyToken, upload);
 router.delete("/delete/:id", verifyToken, deleteUser);
 
 export default router;
