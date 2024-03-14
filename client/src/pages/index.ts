a1b794482c0e0b6b4fb3f09455799cc5be3cdcaa
sanchxt <tpbeast007@gmail.com>
2024-03-14 16:24:53 +0530

    update exports

--- client/src/pages/index.ts
+++ client/src/pages/index.ts
@@ -3,4 +3,5 @@ export { default as Profile } from "./Profile";
 export { default as Home } from "./Dashboard/Home";
 export { default as SignUp } from "./SignUp/SignUp";
 export { default as SignIn } from "./SignIn/SignIn";
+export { default as Prescription } from "./Prescription";
 export { default as UploadPrescription } from "./UploadPrescription";
