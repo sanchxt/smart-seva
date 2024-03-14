5a69efdd4ce5737d7d3e2f3f7c62766117c87eb7
sanchxt <tpbeast007@gmail.com>
2024-03-15 02:19:01 +0530

    update exports

--- client/src/pages/index.ts
+++ client/src/pages/index.ts
@@ -1,5 +1,6 @@
 export { default as About } from "./About";
 export { default as Profile } from "./Profile";
+export { default as Contacts } from "./Contacts";
 export { default as Home } from "./Dashboard/Home";
 export { default as SignUp } from "./SignUp/SignUp";
 export { default as SignIn } from "./SignIn/SignIn";
