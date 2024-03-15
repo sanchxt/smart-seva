578d93c492ff6f1e6affa92493beedca66c3cac2
sanchxt <tpbeast007@gmail.com>
2024-03-16 01:39:19 +0530

    implement add contacts api

--- backend/routes/user.route.js
+++ backend/routes/user.route.js
@@ -6,6 +6,7 @@ import {
   test,
   updateUser,
   upload,
+  addContacts
 } from "../controllers/user.controller.js";
 import { verifyToken } from "../utils/verifyUser.js";
 
@@ -15,6 +16,7 @@ router.get("/", test);
 router.get("/prescription/:id", verifyToken, getPrescription);
 router.post("/upload/:id", verifyToken, upload);
 router.post("/update/:id", verifyToken, updateUser);
+router.post('/add-contacts/:id', verifyToken, addContacts);
 router.delete("/delete/:id", verifyToken, deleteUser);
 
 export default router;
