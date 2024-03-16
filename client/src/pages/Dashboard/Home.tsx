14853b4e31e8a619b8c47144c8ab0dc1d987c664
sanchxt <tpbeast007@gmail.com>
2024-03-16 11:46:57 +0530

    fix sidebar ui

--- client/src/pages/Dashboard/Home.tsx
+++ client/src/pages/Dashboard/Home.tsx
@@ -3,7 +3,7 @@ import Dashboard from "./Dashboard";
 
 const HomePage = () => {
   return (
-    <div className="flex flex-row h-screen">
+    <div className="flex flex-row h-full">
       <SidebarItems activeClass="dashboard" />
       <Dashboard />
     </div>
